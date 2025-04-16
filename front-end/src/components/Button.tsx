4c3c1e83b5abe6ad4c176c6f99c6d5be15889ff6
Deepak <deepakdharsh01@gmail.com>
2025-04-16 20:35:31 +0530

    added some components

--- front-end/src/components/Button.tsx
+++ front-end/src/components/Button.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 
-function Button() {
+type variants= "primary" | "secondary"
+
+interface buttonProps{
+    variant:variants,
+    size:"sm"|"md"|"lg",
+    text:string,
+    startIcon?:ReactElement,
+    endIcon?:ReactElement,
+    onClick?:()=>void
+}
+
+function Button(props:buttonProps) {
+    const variantStyles={
+        "primary":"bg-purple-600 text-white",
+        "secondary":"bg-purple-300 text-purple-600"
+    }
+
+    const sizeStyles={
+        "sm":"p-2 py-1",
+        "md":"p-4 py-2",
+        "lg":"p-6 py-3"
+    }
+
+    const defaultStyles="rounded-md"
+
   return (
-    <div>
-        <button className='text-red-500'>hello</button>
-    </div>
+        <button className={`${variantStyles[props.variant]} ${sizeStyles[props.size]} ${defaultStyles} bg-purple-300`}>{props.text}</button>
   )
 }
 
