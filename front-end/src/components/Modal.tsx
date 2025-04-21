54ba1fb92c9a74d93bd0c305eca7209ca8ff0eb6
Deepak <deepakdharsh01@gmail.com>
2025-04-21 16:38:53 +0530

    front-end completed

--- front-end/src/components/Modal.tsx
+++ front-end/src/components/Modal.tsx
@@ -4,6 +4,7 @@ import Input from './Input'
 
 function Modal({isToggle,handleToggle}):any {
     const inputRef=useRef<HTMLInputElement>(null)
+
     function handleSubmit(){
         console.log(inputRef.current?.value)
         console.log(isToggle)
@@ -11,8 +12,10 @@ function Modal({isToggle,handleToggle}):any {
   return (
     <>
         {isToggle ? (
-            <div className='absolute w-full min-h-screen bg-slate-300 opacity-60 flex justify-center items-center'>
-            <div className='size-100 bg-slate-700 rounded-md p-12 relative'>
+            <>
+            <div onClick={handleToggle} className='fixed z-1 w-full min-h-screen bg-slate-300 opacity-60'>
+            </div>
+            <div className='size-100 z-2  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-700 rounded-md p-12 fixed'>
                 <span onClick={handleToggle} className='absolute top-3 left-91'><CloseIcon/></span>
                 <Input placeholder="title" INPref={inputRef} type='text' />
                 <Input placeholder="title" INPref={inputRef} type='text' />
@@ -21,7 +24,7 @@ function Modal({isToggle,handleToggle}):any {
                 <Input placeholder="title" INPref={inputRef} type='text' />
                 <button onClick={handleSubmit} className=' mt-3 text-white bg-purple-600 px-5 py-2 rounded-xs ml-2'>Submit</button>
             </div>
-        </div>
+            </>
         ) : null}
     </>
 )
